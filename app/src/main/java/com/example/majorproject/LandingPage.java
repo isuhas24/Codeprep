edfe127825ae1f6186e1d3583379669681ca169d
Suhas Indrigi <suhaspoo24@gmail.com>
2023-03-02 19:37:53 +0530

    mobility companies feature implemented

--- app/src/main/java/com/example/majorproject/LandingPage.java
+++ app/src/main/java/com/example/majorproject/LandingPage.java
@@ -1,16 +1,27 @@
 package com.example.majorproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LandingPage extends AppCompatActivity{
+public class LandingPage extends AppCompatActivity implements View.OnClickListener{
+
+    ImageView mobility_image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.landing_page);
+        mobility_image=(ImageView) findViewById(R.id.company1);
+        mobility_image.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v){
+        Intent it=new Intent(this, MobilityCompanies.class);
+        startActivity(it);
+    }
 }
---
+++ app/src/main/java/com/example/majorproject/MobilityCompanies.java
@@ -0,0 +1,16 @@
+package com.example.majorproject;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MobilityCompanies extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mobility_companies);
+    }
+}
