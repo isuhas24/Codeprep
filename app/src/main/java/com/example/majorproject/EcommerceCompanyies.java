f3a7ed14775b92849e615a2e3c100d74864078a5
Deepak <deepaksg888@gmail.com>
2023-03-02 23:30:19 +0530

    forgot password feature implemented in login page

---
+++ app/src/main/java/com/example/majorproject/EcommerceCompanyies.java
@@ -0,0 +1,18 @@
+package com.example.majorproject;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EcommerceCompanyies extends AppCompatActivity implements View.OnClickListener {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.e_commerce_company);
+    }
+    @Override
+    public void onClick(View v) {
+
+    }
+}
