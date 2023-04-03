2e254566350ac1e7431af169cacc16f84cebd5ce
Suhas Indrigi <suhaspoo24@gmail.com>
2023-04-03 13:21:14 +0530

    crm and cloud companies feature implemented, hidden password feature implemented in the activity_main file.

---
+++ app/src/main/java/com/example/majorproject/CloudCompanies.java
@@ -0,0 +1,14 @@
+package com.example.majorproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.BundleCompat;
+
+public class CloudCompanies extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cloud_companies);
+    }
+}
--- app/src/main/java/com/example/majorproject/LandingPage.java
+++ app/src/main/java/com/example/majorproject/LandingPage.java
@@ -1,6 +1,7 @@
 package com.example.majorproject;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -9,7 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class LandingPage extends AppCompatActivity implements View.OnClickListener{
 
-    ImageView mobility_image,ecommerce_image;
+    ImageView mobility_image,ecommerce_image,crm_image,cloud_image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -19,6 +20,10 @@ public class LandingPage extends AppCompatActivity implements View.OnClickListen
         mobility_image.setOnClickListener(this);
         ecommerce_image=(ImageView) findViewById(R.id.company2);
         ecommerce_image.setOnClickListener(this);
+        crm_image=(ImageView) findViewById(R.id.company3);
+        crm_image.setOnClickListener(this);
+        cloud_image=(ImageView) findViewById(R.id.company5);
+        cloud_image.setOnClickListener(this);
     }
 
     @Override
@@ -26,8 +31,17 @@ public class LandingPage extends AppCompatActivity implements View.OnClickListen
         if(v.equals(mobility_image)){
             Intent it=new Intent(this, MobilityCompanies.class);
             startActivity(it);
-        } else if (v.equals(ecommerce_image)) {
+        }
+        else if (v.equals(ecommerce_image)) {
             Intent it=new Intent(this,EcommerceCompanyies.class);
+            startActivity(it);
+        }
+        else if(v.equals(crm_image)){
+            Intent it=new Intent(this,CRMCompanies.class);
+            startActivity(it);
+        }
+        else if(v.equals(cloud_image)){
+            Intent it=new Intent(this,CloudCompanies.class);
             startActivity(it);
         }
     }
