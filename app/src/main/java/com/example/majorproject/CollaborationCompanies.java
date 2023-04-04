312c307496b2367be82a02861701ea3be7c3f3e4
Deepak <deepaksg888@gmail.com>
2023-04-04 09:53:27 +0530

    collaboration companies feature implementation

---
+++ app/src/main/java/com/example/majorproject/CollaborationCompanies.java
@@ -0,0 +1,14 @@
+package com.example.majorproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CollaborationCompanies  extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.collaboration_companies);
+    }
+
+}
