2e6ccd791b0d348d91c0b84e009f4991624722ad
Pradeep <pradeephalemani8@gmail.com>
2023-04-11 09:16:41 +0530

    splash Activity commit

--- app/src/main/java/com/example/majorproject/CProgramming.java
+++ app/src/main/java/com/example/majorproject/CProgramming.java
@@ -1,13 +1,43 @@
 package com.example.majorproject;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class CProgramming extends AppCompatActivity {
+public class CProgramming extends AppCompatActivity implements View.OnClickListener{
+    Button b1,b2,b3,b4,b5,b6,b7,b8,b9,b10;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.c_programming);
+        b1=(Button)findViewById(R.id.c_b1);
+        b1.setOnClickListener(this);
+        b2=(Button)findViewById(R.id.c_b2);
+        b2.setOnClickListener(this);
+        b3=(Button)findViewById(R.id.c_b3);
+        b3.setOnClickListener(this);
+        b4=(Button)findViewById(R.id.c_b4);
+        b4.setOnClickListener(this);
+        b5=(Button)findViewById(R.id.c_b5);
+        b5.setOnClickListener(this);
+        b6=(Button)findViewById(R.id.c_b6);
+        b6.setOnClickListener(this);
+        b7=(Button)findViewById(R.id.c_b7);
+        b7.setOnClickListener(this);
+        b8=(Button)findViewById(R.id.c_b8);
+        b8.setOnClickListener(this);
+        b9=(Button)findViewById(R.id.c_b9);
+        b9.setOnClickListener(this);
+        b10=(Button)findViewById(R.id.c_b10);
+        b10.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
