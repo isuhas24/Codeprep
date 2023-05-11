87bc1aa79229018d93e0bc367a52f5cc76361a7d
Royalxpro <Rina Jana rinajana9629@gmail.com>
2023-05-12 01:42:38 +0530

    5 activity completed video + res + img setted - completly more updates coming ASAP

--- app/src/main/java/com/example/majorproject/SoftwareEngineering.java
+++ app/src/main/java/com/example/majorproject/SoftwareEngineering.java
@@ -1,13 +1,242 @@
 package com.example.majorproject;
 
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.ByteArrayOutputStream;
+
+
 public class SoftwareEngineering extends AppCompatActivity {
+    Button SE_btn1J,SE_btn2J,SE_btn3J,SE_btn4J,SE_btn5J,SE_btn6J,SE_btn7J,SE_btn8J;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.software_engineering);
+
+        SE_btn1J = findViewById(R.id.SE_btn1);
+        SE_btn1J.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String videoId = "b7hmU72GRaE";
+                Resources res = getResources();
+                String Headtxt = "INTRODUCTION TO SOFTWARE ENGINEERING";
+                String Link = "Download Resources PDF From Here: https://drive.google.com/file/d/17InInMGQIGLkr0IRSUAYYGW84arleIWu/view?usp=share_link";
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 2;
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.classpython, options);
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 50, stream);
+                byte[] byteArray = stream.toByteArray();
+                String Imagefinder ="R.drawable.image";
+                Intent intent = new Intent(SoftwareEngineering.this, VideoPlayerActivity.class);
+                intent.putExtra("Link_file", Link);
+                intent.putExtra("video_id", videoId);
+                intent.putExtra("Headtext_id", Headtxt);
+                intent.putExtra("image_finder", Imagefinder);
+                intent.putExtra("image_byte_array", byteArray);
+
+                startActivity(intent);
+
+            }
+        });
+        SE_btn2J = findViewById(R.id.SE_btn2);
+        SE_btn2J.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String videoId = "13nLmF2pLJA";
+                Resources res = getResources();
+                String Headtxt = "LEGACY SOFTWARE";
+                String Link = "Download Resources PDF From Here: https://drive.google.com/file/d/1Ad-JJ4sz9D5Ahynf_-hx3bgV90rwQ-bG/view?usp=share_link";
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 2;
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.classpython, options);
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 50, stream);
+                byte[] byteArray = stream.toByteArray();
+                String Imagefinder ="R.drawable.image";
+                Intent intent = new Intent(SoftwareEngineering.this, VideoPlayerActivity.class);
+                intent.putExtra("Link_file", Link);
+                intent.putExtra("video_id", videoId);
+                intent.putExtra("Headtext_id", Headtxt);
+                intent.putExtra("image_finder", Imagefinder);
+                intent.putExtra("image_byte_array", byteArray);
+
+                startActivity(intent);
+
+            }
+        });
+        SE_btn3J = findViewById(R.id.SE_btn3);
+        SE_btn3J.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String videoId = "kSU2MPeptpM";
+                Resources res = getResources();
+                String Headtxt = "SDLC CYCLE";
+                String Link = "Download Resources PDF From Here: https://drive.google.com/file/d/1D_rih7dNB1YZAvOxhUB5q1OhFx52xhqq/view?usp=sharing";
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 2;
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.classpython, options);
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 50, stream);
+                byte[] byteArray = stream.toByteArray();
+                String Imagefinder ="R.drawable.image";
+                Intent intent = new Intent(SoftwareEngineering.this, VideoPlayerActivity.class);
+                intent.putExtra("Link_file", Link);
+                intent.putExtra("video_id", videoId);
+                intent.putExtra("Headtext_id", Headtxt);
+                intent.putExtra("image_finder", Imagefinder);
+                intent.putExtra("image_byte_array", byteArray);
+
+                startActivity(intent);
+
+            }
+        });
+        SE_btn4J = findViewById(R.id.SE_btn4);
+        SE_btn4J.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String videoId = "6KtqGS5Fdjs";
+                Resources res = getResources();
+                String Headtxt = "SOFTWARE PROCESS MODEL";
+                String Link = "Download Resources PDF From Here: https://drive.google.com/file/d/1pBpel9OQukXpFcgPKde03M02EnTAXNVO/view?usp=share_link";
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 2;
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.classpython, options);
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 50, stream);
+                byte[] byteArray = stream.toByteArray();
+                String Imagefinder ="R.drawable.image";
+                Intent intent = new Intent(SoftwareEngineering.this, VideoPlayerActivity.class);
+                intent.putExtra("Link_file", Link);
+                intent.putExtra("video_id", videoId);
+                intent.putExtra("Headtext_id", Headtxt);
+                intent.putExtra("image_finder", Imagefinder);
+                intent.putExtra("image_byte_array", byteArray);
+
+                startActivity(intent);
+
+            }
+        });
+        SE_btn5J = findViewById(R.id.SE_btn5);
+        SE_btn5J.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String videoId = "JNLRXczA9Y0";
+                Resources res = getResources();
+                String Headtxt = "SOFTWARE  REQUIREMENTS";
+                String Link = "Download Resources PDF From Here: https://drive.google.com/file/d/1m87mw155Uq1ht8bvKoMFW2G1n-tRgWED/view?usp=share_link";
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 2;
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.classpython, options);
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 50, stream);
+                byte[] byteArray = stream.toByteArray();
+                String Imagefinder ="R.drawable.image";
+                Intent intent = new Intent(SoftwareEngineering.this, VideoPlayerActivity.class);
+                intent.putExtra("Link_file", Link);
+                intent.putExtra("video_id", videoId);
+                intent.putExtra("Headtext_id", Headtxt);
+                intent.putExtra("image_finder", Imagefinder);
+                intent.putExtra("image_byte_array", byteArray);
+
+                startActivity(intent);
+
+            }
+        });
+        SE_btn6J = findViewById(R.id.SE_btn6);
+        SE_btn6J.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String videoId = "HpJcVNcugMw";
+                Resources res = getResources();
+                String Headtxt = "SYSTEM MODELING";
+                String Link = "Download Resources PDF From Here: https://drive.google.com/file/d/1_8MfyWE6dJbBeeLc-cDEdCxuYh3WUezb/view?usp=share_link";
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 2;
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.classpython, options);
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 50, stream);
+                byte[] byteArray = stream.toByteArray();
+                String Imagefinder ="R.drawable.image";
+                Intent intent = new Intent(SoftwareEngineering.this, VideoPlayerActivity.class);
+                intent.putExtra("Link_file", Link);
+                intent.putExtra("video_id", videoId);
+                intent.putExtra("Headtext_id", Headtxt);
+                intent.putExtra("image_finder", Imagefinder);
+                intent.putExtra("image_byte_array", byteArray);
+
+                startActivity(intent);
+
+            }
+        });
+        SE_btn7J = findViewById(R.id.SE_btn7);
+        SE_btn7J.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String videoId = "drFxkP3qxqY";
+                Resources res = getResources();
+                String Headtxt = "SOFTWARE DESIGNING CONCEPT";
+                String Link = "Download Resources PDF From Here: https://drive.google.com/file/d/16FEPeO-q1p-miUcOUrLjstFBauBXeaqG/view?usp=share_link";
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 2;
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.classpython, options);
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 50, stream);
+                byte[] byteArray = stream.toByteArray();
+                String Imagefinder ="R.drawable.image";
+                Intent intent = new Intent(SoftwareEngineering.this, VideoPlayerActivity.class);
+                intent.putExtra("Link_file", Link);
+                intent.putExtra("video_id", videoId);
+                intent.putExtra("Headtext_id", Headtxt);
+                intent.putExtra("image_finder", Imagefinder);
+                intent.putExtra("image_byte_array", byteArray);
+
+                startActivity(intent);
+
+            }
+        });
+        SE_btn8J = findViewById(R.id.SE_btn8);
+        SE_btn8J.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String videoId = "OcB1NKX_kYw";
+                Resources res = getResources();
+                String Headtxt = "SOFTWARE ARCHITECTURE";
+                String Link = "Download Resources PDF From Here: https://drive.google.com/file/d/1Y5v3GerNo4_ewd2TTPg0TAjGPIaho68T/view?usp=share_link";
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 2;
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.classpython, options);
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 50, stream);
+                byte[] byteArray = stream.toByteArray();
+                String Imagefinder ="R.drawable.image";
+                Intent intent = new Intent(SoftwareEngineering.this, VideoPlayerActivity.class);
+                intent.putExtra("Link_file", Link);
+                intent.putExtra("video_id", videoId);
+                intent.putExtra("Headtext_id", Headtxt);
+                intent.putExtra("image_finder", Imagefinder);
+                intent.putExtra("image_byte_array", byteArray);
+
+                startActivity(intent);
+
+            }
+        });
+
+
+
+
+
+
+
     }
 }
