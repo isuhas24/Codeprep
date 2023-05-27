645d135b18b0160b8d4b546e4835d7dea2826df8
Royalxpro <Rina Jana rinajana9629@gmail.com>
2023-05-28 00:36:18 +0530

    ALL ACTIVITY HASBEEN UPDATED WITH VIDEO AND RESOURCES
    RESUME MAKER IS COOKING

--- app/src/main/java/com/example/majorproject/SQLResources.java
+++ app/src/main/java/com/example/majorproject/SQLResources.java
@@ -1,43 +1,281 @@
 package com.example.majorproject;
 
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SQLResources extends AppCompatActivity implements View.OnClickListener {
+import java.io.ByteArrayOutputStream;
+
+public class SQLResources extends AppCompatActivity  {
     Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn10;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sql_resources);
+
+
+
+
+
+
+        // G drive update needed
         btn1=(Button) findViewById(R.id.SQLResourcesCustomButton1);
-        btn1.setOnClickListener(this);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String videoId = "Gzk_wR-ptck";
+                Resources res = getResources();
+                String Headtxt = "FUNDAMENTALS OF SQL";
+                String Link = "Download Resources PDF From Here: https://drive.google.com/file/d/1zUOlW-LMO88UtJ4sVkxcs0UFupvTnwE_/view?usp=share_link";
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 2;
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.sqlfundamentle, options);
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 50, stream);
+                byte[] byteArray = stream.toByteArray();
+                String Imagefinder ="R.drawable.image";
+                Intent intent = new Intent(SQLResources.this, VideoPlayerActivity.class);
+                intent.putExtra("Link_file", Link);
+                intent.putExtra("video_id", videoId);
+                intent.putExtra("Headtext_id", Headtxt);
+                intent.putExtra("image_finder", Imagefinder);
+                intent.putExtra("image_byte_array", byteArray);
+
+                startActivity(intent);
+            }
+        });
         btn2=(Button) findViewById(R.id.SQLResourcesCustomButton2);
-        btn2.setOnClickListener(this);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String videoId = "oNP0kwvjo2k";
+                Resources res = getResources();
+                String Headtxt = "AGGREGRATE COMMANDS";
+                String Link = "Download Resources PDF From Here: https://drive.google.com/file/d/1zUOlW-LMO88UtJ4sVkxcs0UFupvTnwE_/view?usp=share_link";
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 2;
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.sqlagregate, options);
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 50, stream);
+                byte[] byteArray = stream.toByteArray();
+                String Imagefinder ="R.drawable.image";
+                Intent intent = new Intent(SQLResources.this, VideoPlayerActivity.class);
+                intent.putExtra("Link_file", Link);
+                intent.putExtra("video_id", videoId);
+                intent.putExtra("Headtext_id", Headtxt);
+                intent.putExtra("image_finder", Imagefinder);
+                intent.putExtra("image_byte_array", byteArray);
+
+                startActivity(intent);
+            }
+        });
         btn3=(Button) findViewById(R.id.SQLResourcesCustomButton3);
-        btn3.setOnClickListener(this);
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String videoId = "LkdNwTrnDks";
+                Resources res = getResources();
+                String Headtxt = "ALIAS";
+                String Link = "Download Resources PDF From Here: https://drive.google.com/file/d/1zUOlW-LMO88UtJ4sVkxcs0UFupvTnwE_/view?usp=share_link";
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 2;
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.sqlalias, options);
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 50, stream);
+                byte[] byteArray = stream.toByteArray();
+                String Imagefinder ="R.drawable.image";
+                Intent intent = new Intent(SQLResources.this, VideoPlayerActivity.class);
+                intent.putExtra("Link_file", Link);
+                intent.putExtra("video_id", videoId);
+                intent.putExtra("Headtext_id", Headtxt);
+                intent.putExtra("image_finder", Imagefinder);
+                intent.putExtra("image_byte_array", byteArray);
+
+                startActivity(intent);
+            }
+        });
         btn4=(Button) findViewById(R.id.SQLResourcesCustomButton4);
-        btn4.setOnClickListener(this);
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String videoId = "CIxKN05KG4Q";
+                Resources res = getResources();
+                String Headtxt = "CONDITIONAL STATEMENTS";
+                String Link = "Download Resources PDF From Here: https://drive.google.com/file/d/1zUOlW-LMO88UtJ4sVkxcs0UFupvTnwE_/view?usp=share_link";
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 2;
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.sqlcondit, options);
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 50, stream);
+                byte[] byteArray = stream.toByteArray();
+                String Imagefinder ="R.drawable.image";
+                Intent intent = new Intent(SQLResources.this, VideoPlayerActivity.class);
+                intent.putExtra("Link_file", Link);
+                intent.putExtra("video_id", videoId);
+                intent.putExtra("Headtext_id", Headtxt);
+                intent.putExtra("image_finder", Imagefinder);
+                intent.putExtra("image_byte_array", byteArray);
+
+                startActivity(intent);
+            }
+        });
         btn5=(Button) findViewById(R.id.SQLResourcesCustomButton5);
-        btn5.setOnClickListener(this);
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String videoId = "uFYnTMjwStc";
+                Resources res = getResources();
+                String Headtxt = "FILTERING COMMANDS";
+                String Link = "Download Resources PDF From Here: https://drive.google.com/file/d/1zUOlW-LMO88UtJ4sVkxcs0UFupvTnwE_/view?usp=share_link";
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 2;
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.sqlfiltering, options);
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 50, stream);
+                byte[] byteArray = stream.toByteArray();
+                String Imagefinder ="R.drawable.image";
+                Intent intent = new Intent(SQLResources.this, VideoPlayerActivity.class);
+                intent.putExtra("Link_file", Link);
+                intent.putExtra("video_id", videoId);
+                intent.putExtra("Headtext_id", Headtxt);
+                intent.putExtra("image_finder", Imagefinder);
+                intent.putExtra("image_byte_array", byteArray);
+
+                startActivity(intent);
+            }
+        });
         btn6=(Button) findViewById(R.id.SQLResourcesCustomButton6);
-        btn6.setOnClickListener(this);
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String videoId = "6gASpEFZ5Is";
+                Resources res = getResources();
+                String Headtxt = "GROUP BY COMMANDS";
+                String Link = "Download Resources PDF From Here: https://drive.google.com/file/d/1zUOlW-LMO88UtJ4sVkxcs0UFupvTnwE_/view?usp=share_link";
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 2;
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.sqlgroupby, options);
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 50, stream);
+                byte[] byteArray = stream.toByteArray();
+                String Imagefinder ="R.drawable.image";
+                Intent intent = new Intent(SQLResources.this, VideoPlayerActivity.class);
+                intent.putExtra("Link_file", Link);
+                intent.putExtra("video_id", videoId);
+                intent.putExtra("Headtext_id", Headtxt);
+                intent.putExtra("image_finder", Imagefinder);
+                intent.putExtra("image_byte_array", byteArray);
+
+                startActivity(intent);
+            }
+        });
         btn7=(Button) findViewById(R.id.SQLResourcesCustomButton7);
-        btn7.setOnClickListener(this);
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String videoId = "vncBSUNb4NA";
+                Resources res = getResources();
+                String Headtxt = "JOINS";
+                String Link = "Download Resources PDF From Here: https://drive.google.com/file/d/1zUOlW-LMO88UtJ4sVkxcs0UFupvTnwE_/view?usp=share_link";
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 2;
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.sqljoins, options);
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 50, stream);
+                byte[] byteArray = stream.toByteArray();
+                String Imagefinder ="R.drawable.image";
+                Intent intent = new Intent(SQLResources.this, VideoPlayerActivity.class);
+                intent.putExtra("Link_file", Link);
+                intent.putExtra("video_id", videoId);
+                intent.putExtra("Headtext_id", Headtxt);
+                intent.putExtra("image_finder", Imagefinder);
+                intent.putExtra("image_byte_array", byteArray);
+
+                startActivity(intent);
+            }
+        });
         btn8=(Button) findViewById(R.id.SQLResourcesCustomButton8);
-        btn8.setOnClickListener(this);
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String videoId = "yH0Ek5aSxv8";
+                Resources res = getResources();
+                String Headtxt = "MATHEMATICAL FUNCTIONS";
+                String Link = "Download Resources PDF From Here: https://drive.google.com/file/d/1zUOlW-LMO88UtJ4sVkxcs0UFupvTnwE_/view?usp=share_link";
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 2;
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.sqlmath, options);
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 50, stream);
+                byte[] byteArray = stream.toByteArray();
+                String Imagefinder ="R.drawable.image";
+                Intent intent = new Intent(SQLResources.this, VideoPlayerActivity.class);
+                intent.putExtra("Link_file", Link);
+                intent.putExtra("video_id", videoId);
+                intent.putExtra("Headtext_id", Headtxt);
+                intent.putExtra("image_finder", Imagefinder);
+                intent.putExtra("image_byte_array", byteArray);
+
+                startActivity(intent);
+            }
+        });
         btn9=(Button) findViewById(R.id.SQLResourcesCustomButton9);
-        btn9.setOnClickListener(this);
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String videoId = "tQXrEzxuf6A";
+                Resources res = getResources();
+                String Headtxt = "ORDERING COMMANDS";
+                String Link = "Download Resources PDF From Here: https://drive.google.com/file/d/1zUOlW-LMO88UtJ4sVkxcs0UFupvTnwE_/view?usp=share_link";
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 2;
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.sqlordering, options);
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 50, stream);
+                byte[] byteArray = stream.toByteArray();
+                String Imagefinder ="R.drawable.image";
+                Intent intent = new Intent(SQLResources.this, VideoPlayerActivity.class);
+                intent.putExtra("Link_file", Link);
+                intent.putExtra("video_id", videoId);
+                intent.putExtra("Headtext_id", Headtxt);
+                intent.putExtra("image_finder", Imagefinder);
+                intent.putExtra("image_byte_array", byteArray);
+
+                startActivity(intent);
+            }
+        });
         btn10=(Button) findViewById(R.id.SQLResourcesCustomButton10);
-        btn10.setOnClickListener(this);
+        btn10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String videoId = "55_UN5gVARs";
+                Resources res = getResources();
+                String Headtxt = "STRING FUNCTIONS";
+                String Link = "Download Resources PDF From Here: https://drive.google.com/file/d/1zUOlW-LMO88UtJ4sVkxcs0UFupvTnwE_/view?usp=share_link";
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 2;
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.sqlstring, options);
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 50, stream);
+                byte[] byteArray = stream.toByteArray();
+                String Imagefinder ="R.drawable.image";
+                Intent intent = new Intent(SQLResources.this, VideoPlayerActivity.class);
+                intent.putExtra("Link_file", Link);
+                intent.putExtra("video_id", videoId);
+                intent.putExtra("Headtext_id", Headtxt);
+                intent.putExtra("image_finder", Imagefinder);
+                intent.putExtra("image_byte_array", byteArray);
+
+                startActivity(intent);
+            }
+        });
     }
 
-    @Override
-    public void onClick(View v){
-        if(v.equals(btn1)){
 
-        }
-    }
 }
